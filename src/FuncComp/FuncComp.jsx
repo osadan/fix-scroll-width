7f5bbb56bef6c6f8ae2ce74a8469f52fa53261ea
Ohad Sadan <sadanoh@gmail.com>
2020-01-28 10:31:22 +0200

    fix(function component): use forwardRef

--- src/FuncComp/FuncComp.jsx
+++ src/FuncComp/FuncComp.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Styled from '../Comp1/styled';
 import ScrollFix from '../ScrollFix/ScrollFix';
 
-const FuncComp = (props, ref) => {
-  console.log(props, ref);
+const FuncComp = (props) => {
   return (
-    <React.Fragment>
-      <Styled.Wrapper >
+    <React.Fragment >
+      <Styled.Wrapper ref={props.forwardRef}>
         <Styled.Test>Test content</Styled.Test>
       </Styled.Wrapper>
     </React.Fragment>
--- src/ScrollFix/ScrollFix.jsx
+++ src/ScrollFix/ScrollFix.jsx
@@ -7,38 +7,45 @@ const ScrollFixHoc = (WrappedComponent) => {
   class ScrollFix extends Component {
     constructor(props) {
       super(props)
-      this.ref = {}//React.createRef();
+      this.ref = React.createRef();
     }
+
     componentDidMount() {
-      console.log(this.ref);
-      console.log(this.props.forwardRef);
-      console.log(this.props.children);
-      console.log(React.Children.map(i => console.log(i)));
       if (!this.ref || !this.ref.current) {
         return;
       }
+
       const element = ReactDOM.findDOMNode(this.ref.current);
-      console.log(element);
+      if (!elememt) {
+        return;
+      }
+
       if (element.scrollHeight > element.clientHeight) {
         const { paddingLeft, marginLeft, marginRight, paddingRight, borderLeftWidth, borderRightWidth } = getComputedStyle(element);
         const scrollPadding = element.offsetWidth - parseInt(paddingRight) - parseInt(marginLeft) - parseInt(marginRight) - parseInt(borderLeftWidth) - parseInt(borderRightWidth) - element.clientWidth;
         element.style.paddingLeft = (parseInt(paddingLeft) + scrollPadding) + 'px';
       }
     }
-    render() {
 
-      return (
-        <React.Fragment>
-          <WrappedComponent tmp="3" ref={this.ref} {...this.props}></WrappedComponent>
-        </React.Fragment>
+    isClassComponent(component) {
+      return !!(
+        typeof component === 'function'
+        && component.prototype
+        && component.prototype.isReactComponent
       );
+    }
+    render() {
+      if (this.isClassComponent(WrappedComponent)) {
+        return <WrappedComponent {...this.props} ref={this.ref} />
+      } else {
+        return <WrappedComponent {...this.props} forwardRef={this.ref} />
+      }
     }
   }
 
-  //return React.forwardRef((props, ref) => {
-  //return <ScrollFix {...props} forwardRef={ref} />
-  //})
-  return ScrollFix;
+  return React.forwardRef((props, ref) => {
+    return <ScrollFix {...props} forwardRef={ref} />
+  })
 }
 
 export default ScrollFixHoc;
